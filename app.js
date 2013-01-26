fb89d7153e6606e0a4bf5f6f09cbef66dc1eae6d
jialiya <jialiya.huang@students.olin.edu>
2013-01-26 10:57:08 -0500

    code after class

---
+++ app.js
@@ -0,0 +1,66 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+
+  //new code 
+  , mongoose = require('mongoose')
+// new code
+
+  , path = require('path');
+
+var app = express();
+// new code
+mongoose.connect(process.env.MONGOLAB_URI || 'localhost');
+
+var schema = mongoose.Schema({ name: String });
+var Cat = mongoose.model('Cat', schema);
+
+
+
+// new code
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/users/new', user.new);
+// new code
+app.get('/cats/new', function (req, res) {
+  var kitty = new Cat({ name: 'Zildjian' });
+  kitty.save(function (err) {
+    if (err) 
+      return console.log("error", err);
+    res.send('meow');
+  });
+});
+// end new code
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+
+
+
+
